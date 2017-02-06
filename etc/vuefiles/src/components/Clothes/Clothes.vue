9f36dbd5b7ddfbf064e1136ac5927e942f92499b
ahaha0807 <b5331@oic.jp>
2017-02-07 01:27:04 +0900

    インナー表示ボタンの追加

--- etc/vuefiles/src/components/Clothes/Clothes.vue
+++ etc/vuefiles/src/components/Clothes/Clothes.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="top__clothes" :class="{ 'top__clothes-toggle': isTurn }" @click="doTurn">
+    <div class="top__clothes">
+      <div class="top__clothes-content" :class="{ 'top__clothes-back-side': isTurn }" @click="doTurn">
         <div class="top__clothes-front">
           <ClothHeader></ClothHeader>
           <ClothImages :clothes="clothes"></ClothImages>
         </div>
         <ClothesDescription :dayDescription="clothes.day_clothes_description" :nightDescription="clothes.night_clothes_description"></ClothesDescription>
+      </div>
+      <OuterToggleButton v-on:hideOuter="hideOuter" v-on:indicateOuter="indicateOuter" :manOuterData="clothes.man_clothes.outer_image"></OuterToggleButton>        
     </div>
 </template>
 
@@ -12,13 +15,15 @@
 import ClothHeader from './ClothHeader.vue'
 import ClothImages from './ClothImages.vue'
 import ClothesDescription from './ClothesDescription.vue'
+import OuterToggleButton from './OuterToggleButton.vue'
 
 export default {
   props: ['clothes'],
   components: {
     ClothHeader,
     ClothImages,
-    ClothesDescription
+    ClothesDescription,
+    OuterToggleButton
   },
   methods: {
     doTurn: function () {
@@ -27,11 +32,43 @@ export default {
       } else {
         this.isTurn = false
       }
+    },
+    hideOuter: function () {
+      // OuterToggleButton からhideOuterのemit（通知）が来た場合に呼ばれる
+      if (this.isTurn) {
+        // 裏側を向いていた場合、表に向ける
+        this.isTurn = false
+      }
+
+      // アウターのファイル名を退避
+      this.tmp_man_outer = this.clothes.man_clothes.outer_image
+      this.tmp_woman_outer = this.clothes.woman_clothes.outer_image
+
+      // アウターの画像ファイル名をクリアして非表示にする
+      this.clothes.man_clothes.outer_image = ''
+      this.clothes.woman_clothes.outer_image = ''
+    },
+    indicateOuter: function () {
+      // OuterToggleButton からindicateOuterのemit（通知）が来た場合に呼ばれる
+      if (this.isTurn && this.clothes.man_clothes.outer_image === '') {
+        // 裏側を向いているかつアウターが非表示になっていた場合、表に向ける
+        this.isTurn = false
+      }
+
+      // 退避させていたアウターのファイル名を復帰
+      this.clothes.man_clothes.outer_image = this.tmp_man_outer
+      this.clothes.woman_clothes.outer_image = this.tmp_woman_outer
+
+      // アウターファイル名の退避場所をクリア
+      this.tmp_man_outer = ''
+      this.tmp_woman_outer = ''
     }
   },
   data () {
     return {
-      isTurn: false
+      isTurn: false,  // True だと裏返っている（説明文が表示されている）
+      tmp_man_outer: '',
+      tmp_woman_outer: ''
     }
   }
 }
@@ -39,20 +76,28 @@ export default {
 
 <style>
 .top__clothes {
-    height: 60%;
+    height: 70%;
     position: relative;
     -webkit-transform-style: preserve-3d;
     transform-style: preserve-3d;
     -webkit-transition: -webkit-transform .6s;
     transition: .6s;
 }
-.top__clothes-toggle {
+.top__clothes-content {
+    height: 100%;
+    position: relative;
+    -webkit-transform-style: preserve-3d;
+    transform-style: preserve-3d;
+    -webkit-transition: -webkit-transform .6s;
+    transition: .6s;
+}
+
+.top__clothes-back-side {
     -ms-transform:rotateY(180deg);
     -webkit-transform: rotateY(180deg);
     transform: rotateY(180deg);
 }
 .top__clothes-front {
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
+    height: 100%;
 }
 </style>
---
+++ etc/vuefiles/src/components/Clothes/OuterToggleButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="manOuterData != '' || isDisplay == false" class="top__clothes-outer-toggle-button" @click="outerToggle">
+    Inner を表示
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['manOuterData'],
+    methods: {
+      outerToggle: function () {
+        if (this.isDisplay) {
+          this.isDisplay = false
+          this.$emit('hideOuter')
+        } else {
+          this.isDisplay = true
+          this.$emit('indicateOuter')
+        }
+      }
+    },
+    data () {
+      return {
+        isDisplay: true
+      }
+    }
+  }
+</script>
+
+<style>
+.top__clothes-outer-toggle-button {
+  background-color: deepskyblue;
+  width: 100%;
+  height: 10%;
+  position: relative;
+  top: -10%;
+  font-size: 4em;
+  color: #ffffff;
+  text-align: center;
+}
+</style>
