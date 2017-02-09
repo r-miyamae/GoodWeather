acac790bc0fb4814008f2dbbe1c59654caaa7e45
r-miyamae <b5005@oic.jp>
2017-02-10 02:36:17 +0900

    signin処理の実装

--- src/main/java/GoodWeather/api/UserApi.java
+++ src/main/java/GoodWeather/api/UserApi.java
@@ -3,6 +3,9 @@ package GoodWeather.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.sql.*;
 
 @RestController
@@ -59,5 +62,56 @@ public class UserApi {
             }
         }
             return ResponseEntity.status(402);
+    }
+
+
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public ResponseEntity.BodyBuilder signin(HttpServletRequest request,
+                                             HttpServletResponse response,
+                                             @RequestParam("mailAddress") String mailAddress,
+                                             @RequestParam("password") String password){
+        //DBに接続
+        Connection connection = null;
+        Statement statement = null;
+        PreparedStatement ps = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+            statement = connection.createStatement();
+
+            String userInfo = "select * from users_table where mailAddress = ? and password = ?";
+            ps = connection.prepareStatement(userInfo);
+            ps.setString(1, mailAddress);
+            ps.setString(2, password);
+
+            ResultSet result = ps.executeQuery();
+            if (result.next() != false){
+                HttpSession session = request.getSession(true);
+                session.setAttribute("mailAddress",mailAddress);
+                System.out.println(session.getAttribute("mailAddress"));
+                return ResponseEntity.status(200);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return ResponseEntity.status(402);
     }
 }
