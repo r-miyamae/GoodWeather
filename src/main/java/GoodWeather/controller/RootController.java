82b8975fb0f73b9a2977dbd9c33cf0d9b61f1c03
r-miyamae <b5005@oic.jp>
2017-02-10 22:16:54 +0900

    topのルーティング変更

--- src/main/java/GoodWeather/controller/RootController.java
+++ src/main/java/GoodWeather/controller/RootController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RootController {
     @RequestMapping("/")
     public String index() {
-     return "redirect:/#/top";
+     return "/index.html";
     }
 }
